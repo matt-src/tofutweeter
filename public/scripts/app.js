286430369c96f98674278e16869ba23d8b79b34a
tofurocks <mattwall0@gmail.com>
2018-09-16 20:53:16 -0700

    removed console logs and junk code, added comments

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,6 +19,7 @@ const tweetData = {
     },
     "created_at": 1461116232227
   }
+//Load our tweets from the db through our /tweets GET endpoint ( calls renderTweets() on success )
 function loadTweets(){
     $.get("/tweets", function(res){
         renderTweets(res)
@@ -32,30 +33,34 @@ function loadTweets(){
         $composeButton.css("background-color", "white");
     })
 }
-
+//Return a container with all of the tweets passed
 function renderTweets(tweets){
     for(i = 0; i < tweets.length; i++){
         let $newTweet = createTweetElement(tweets[i])
         $('#tweets-container').append($newTweet);
     }
 }
+
+//Create a new tweet and return it as a jquery object
 function createTweetElement(tweet){
+    //Pull data from tweet object passed as argument
     let usr = tweet["user"];
     let content = tweet["content"];
     let created = tweet["created_at"];
-    let timeCreated = new Date(created)
-    let formattedTime = formatTime(timeCreated);
+    let timeCreated = new Date(created) //Convert time created from ms to a Date object
+    let formattedTime = formatTime(timeCreated); //Format the time to form '2 days ago' , '3 hours ago' etc.
+    //Assign properties of the tweet user to variables
     let imgURL = usr.avatars.small;
     let handle = usr.handle;
     let name = usr.name;
     let img = "<img src=\"" + imgURL + "\" >";
+    //Create a jquery object for each part of our tweet
     let $result = $("<article>").addClass("tweet");
     let $tweetHead = $('<header></header>').addClass("tweetHead");
     let $tweetBody = $('<article></article>').addClass("tweetBody");
     let $tweetFooter = $('<footer></footer>').addClass("tweetFooter");
     let $avatar = $("<span>" + img + "</span>").addClass("avatar");
     let $created = $("<article>" + formatTime(timeCreated) + "</article>").addClass("created");
-    $avatar.css({"padding-right": "5px"});
     let $name = $("<span> " + name + " </span>").addClass("name");
     let $handle = $("<span> " + handle + " </span>").addClass("handle");
     let $emojis = $("<span>🏴</span><span>♻️</span><span>💕</span>").addClass("emojis");
